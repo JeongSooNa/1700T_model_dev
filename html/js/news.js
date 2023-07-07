324537d64d889b826aa02a3aa0117d8e0b260167
JeongSoo Na <skwjdtn3171@naver.com>
2023-07-07 16:25:03 +0900

    feat js

--- html/js/news.js
+++ html/js/news.js
@@ -57,6 +57,71 @@ var lineChart = new Chart(ctx1, {
 
 // Load RCSB released csv data
 $.ajax({
-  url: "https://raw.githubusercontent.com/JeongSooNa/1700T_model_dev/main/html/data/1700T_raw_final.csv",
+  url: "https://raw.githubusercontent.com/JeongSooNa/1700T_model_dev/main/html/data/RCSB_released_2023_total.csv",
   dataType: "text",
 }).done(successFunction);
+// Column
+/* 
+  0. EntryID
+  1. Date
+  2. Ligand
+  3. Value
+  4. Symbol
+  5. Type
+  6. Unit
+  7. PDBID
+  8. Title
+  9. StructureTitle
+  10. StuctureKeywords
+  11. GeneName
+  12. ECNumber
+  13. AnnotationIdentifier
+  14. PolymerEntityID
+  15. EntryIdPolymerEntityIdentifiers
+  16. TargetProtein
+  17. AccessionCode
+  18. DatabaseName
+  19. LigandFormula
+  20. LigandMW
+  21. LigandID
+  22. LigandName
+  23. LigandSMILES
+  24. NonPolymerEntityID
+*/
+function successFunction(data){
+  // console.log("Load Data Complete!")
+  // console.log(data)
+  var colList = data.split("\n");
+  var len = colList.length;
+  var column = colList[0].split(",");
+  // console.log(column);
+  // console.log(len);
+  // console.log(colList[1]);
+  // console.log(colList[2].split(",")[0])
+  function loadTable() {
+    // table clear
+    $("#rrd-data-data tbody *").remove();
+    // load data
+    var html = "";
+    for (var i = 1; i < 100; i++) {
+
+      // append table content
+      html +=
+        "<tr><td>" +
+        colList[i].split(",")[0] + // PDB
+        "</td><td>" +
+        colList[i].split(",")[11] + // Gene Name
+        "</td><td>" +
+        colList[i].split(",")[16] + // Mutant Type
+        "</td><td>" +
+        colList[i].split(",")[17] + // Uniprot ID
+        "</td><td>" +
+        colList[i].split(",")[22] + // Ligand
+        "</td><td>" +
+        colList[i].split(",")[20] + // Ligand MW
+        "</td></tr>";
+    }
+    $("#rrd-data-data tbody").append(html);
+  }
+  loadTable();
+}
